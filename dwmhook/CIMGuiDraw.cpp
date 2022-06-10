4eed5cecf0eaf9b9b8c1612d17640cb301e473a2
ilovecsad <78861284+ilovecsad@users.noreply.github.com>
2022-06-10 19:02:29 +0800

    Add files via upload

---
+++ dwmhook/CIMGuiDraw.cpp
@@ -0,0 +1,195 @@
+#include "CIMGuiDraw.h"
+#define MAPINGLEN 144008
+volatile LONG g_isLocked = FALSE;
+
+extern HANDLE hEvent;;
+
+CIMGuiDraw::CIMGuiDraw()
+{
+}
+
+CIMGuiDraw::~CIMGuiDraw()
+{
+}
+
+bool CIMGuiDraw::InitMiGuiDx11(IDXGISwapChain* pSwapChain, ID3D11Device* pd3dDevice)
+{
+	return ImGuiDx11_init(pSwapChain, pd3dDevice);
+}
+
+void CIMGuiDraw::ImGuiDx11Draw()
+{
+	//当前数组 图形数量检查
+	if ((m_pDrawAll->m_DrawDll != true))
+	{
+		return;
+	}
+
+	//begin
+	ImGui_ImplDX11_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+
+	//画所有图形
+	for (int i = 0; i < m_pDrawAll->m_DrawCount; i++)
+	{
+		switch (m_pDrawAll->m_DrawInfoArr[i].m_DrawType)
+		{
+		case Text_M:
+		{
+			DrawTextStr& Text = m_pDrawAll->m_DrawInfoArr[i].From.Text;
+			DrawNewText(Text.m_X, Text.m_Y, Text.m_Color, Text.m_Outlined, Text.m_Str);
+		}
+			break;
+		case Text2_M:
+		{
+			DrawTextStr& Text = m_pDrawAll->m_DrawInfoArr[i].From.Text;
+			DrawNewTextStr(Text.m_X, Text.m_Y, Text.m_Color, Text.m_Outlined, Text.m_Str);
+		}
+			break;
+		case CircleFill_M:
+		{
+			DrawCircleFillStr& CircleFill = m_pDrawAll->m_DrawInfoArr[i].From.CircleFill;
+			DrawCircleFilled(CircleFill.m_X, CircleFill.m_Y, CircleFill.m_Radius, CircleFill.m_Color, CircleFill.m_Segments);
+		}
+			break;
+		case Circle_M:
+		{
+			DrawCircleStr& Circle = m_pDrawAll->m_DrawInfoArr[i].From.Circle;
+			DrawCircle(Circle.m_X, Circle.m_Y, Circle.m_Radius, Circle.m_Color, Circle.m_Segments, Circle.m_thickness);
+		}
+			break;
+		case Rect_M:
+		{
+			DrawRectStr& Rect = m_pDrawAll->m_DrawInfoArr[i].From.Rect;
+			DrawRect(Rect.m_X, Rect.m_Y, Rect.m_W, Rect.m_H, Rect.m_Color, Rect.m_thickness);
+		}
+			break;
+		case RectEx_M:
+		{
+			DrawRectExStr& RectEx = m_pDrawAll->m_DrawInfoArr[i].From.RectEx;
+			DrawRectEx(RectEx.m_X, RectEx.m_Y, RectEx.m_W, RectEx.m_H, RectEx.m_Color, RectEx.m_thickness);
+		}
+			break;
+		case FilledRect_M:
+		{
+			DrawFilledRectStr& FilledRect = m_pDrawAll->m_DrawInfoArr[i].From.FilledRect;
+			DrawFilledRect(FilledRect.m_X, FilledRect.m_Y, FilledRect.m_W, FilledRect.m_H, FilledRect.m_Color);
+		}
+			break;
+		case Line_M:
+		{
+			DrawLineStr& Line = m_pDrawAll->m_DrawInfoArr[i].From.Line;
+			DrawLine(Line.m_X1, Line.m_Y1, Line.m_X2, Line.m_Y2, Line.m_Color, Line.thickness);
+		}
+			break;
+		case FPS_M:
+		{
+			FPS& Fps = m_pDrawAll->m_DrawInfoArr[i].From.Fps;
+			GetFps(Fps.Fps);
+		}
+		break;
+		default:
+			break;
+		}
+	}
+
+	//OutputDebugPrintf("hzw:currentThread:%d  Count:%d\n", GetCurrentThreadId(), m_pDrawAll->m_DrawCount);
+	//画图标志检查
+	m_pDrawAll->m_DrawCount = 0;
+	m_pDrawAll->m_DrawDll = false;
+
+	//end
+	ImGui::Render();
+	g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+	//解决重叠 问题
+	SetEvent(hEvent);
+
+}
+
+bool CIMGuiDraw::InitMessage()
+{
+	bool flag = false;
+
+	//共享内存
+	flag = InitFileMapping();
+
+	return flag;
+}
+
+bool CIMGuiDraw::InitFileMapping()
+{
+	SECURITY_ATTRIBUTES sa = { 0 };
+	SECURITY_DESCRIPTOR sd = { 0 };
+	InitializeSecurityDescriptor(&sd, SECURITY_DESCRIPTOR_REVISION);
+	SetSecurityDescriptorDacl(&sd, TRUE, NULL, FALSE);
+	sa.bInheritHandle = FALSE;
+	sa.lpSecurityDescriptor = &sd;
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+	HANDLE hFileMapping = CreateFileMappingW(INVALID_HANDLE_VALUE, &sa, PAGE_READWRITE, 0, MAPINGLEN, MAPNAME);
+	m_pDrawAll = (PDRWARR)MapViewOfFile(hFileMapping, FILE_MAP_WRITE | FILE_MAP_READ, 0, 0, 0);
+	if (hFileMapping == NULL)
+	{
+		CK_TRACE_INFO("hzw:OpenFileMappingA:%d\n", GetLastError());
+		return false;
+	}
+
+	CK_TRACE_INFO("hzw:m_pDrawAll:%llx\n", m_pDrawAll);
+
+	if (m_pDrawAll == NULL)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+
+VOID EnterSpinLock(VOID)
+{
+	SIZE_T spinCount = 0;
+
+	// Wait until the flag is FALSE.
+	while (InterlockedCompareExchange(&g_isLocked, TRUE, FALSE) != FALSE)
+	{
+		// No need to generate a memory barrier here, since InterlockedCompareExchange()
+		// generates a full memory barrier itself.
+
+		// Prevent the loop from being too busy.
+		if (spinCount < 32)
+			Sleep(0);
+		else
+			Sleep(1);
+
+		spinCount++;
+	}
+}
+
+//-------------------------------------------------------------------------
+VOID LeaveSpinLock(VOID)
+{
+	// No need to generate a memory barrier here, since InterlockedExchange()
+	// generates a full memory barrier itself.
+
+	InterlockedExchange(&g_isLocked, FALSE);
+}
+
+//-------------------------------------------------------------------------
+
+
+void OutputDebugPrintf(const char* strOutputString, ...)
+{
+#define PUT_PUT_DEBUG_BUF_LEN   256
+	char strBuffer[PUT_PUT_DEBUG_BUF_LEN] = { 0 };
+	va_list vlArgs;
+	va_start(vlArgs, strOutputString);
+	_vsnprintf_s(strBuffer, sizeof(strBuffer) - 1, strOutputString, vlArgs);  //_vsnprintf_s  _vsnprintf
+	//vsprintf(strBuffer,strOutputString,vlArgs);
+	va_end(vlArgs);
+	OutputDebugStringA(strBuffer);  //OutputDebugString    // OutputDebugStringW
+
+}
---
+++ dwmhook/main.cpp
@@ -0,0 +1,133 @@
+#include "includes.hpp"
+#include "ImGui\imgui.h"
+#include "ImGui\imgui_internal.h"
+#include "ImGui\imgui_impl_dx11.h"
+#include "IMGUI/MyImGui.h"
+#include "ck.h"
+#include "CIMGuiDraw.h"
+CIMGuiDraw g_ImGuiDraw;
+HANDLE hEvent = NULL;
+
+BOOL bDataCompare( const BYTE* pData, const BYTE* bMask, const char* szMask )
+{
+	for ( ; *szMask; ++szMask, ++pData, ++bMask )
+	{
+		if ( *szMask == 'x' && *pData != *bMask )
+			return FALSE;
+	}
+	return ( *szMask ) == NULL;
+}
+
+DWORD64 FindPattern( const char* szModule, BYTE* bMask, const char* szMask )
+{
+	MODULEINFO mi{ };
+	GetModuleInformation( GetCurrentProcess(), GetModuleHandleA( szModule ), &mi, sizeof( mi ) );
+
+	DWORD64 dwBaseAddress = DWORD64( mi.lpBaseOfDll );
+	const auto dwModuleSize = mi.SizeOfImage;
+
+	for ( auto i = 0ul; i < dwModuleSize; i++ )
+	{
+		if ( bDataCompare( PBYTE( dwBaseAddress + i ), bMask, szMask ) )
+			return DWORD64( dwBaseAddress + i );
+	}
+	return NULL;
+}
+
+void DrawEverything( IDXGISwapChain* pDxgiSwapChain )
+{
+	static bool b = true;
+	if ( b )
+	{
+		ID3D11Device* pDevice = NULL;
+		pDxgiSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)&pDevice);
+		//初始化migui
+		if (g_ImGuiDraw.InitMiGuiDx11(pDxgiSwapChain, pDevice))
+		{
+			CK_TRACE_INFO("ImGui初始化成功!\n");
+		}
+
+		if (g_ImGuiDraw.InitMessage())
+		{
+			CK_TRACE_INFO("通讯初始化成功!\n");
+		}
+		b = false;
+	
+		hEvent = CreateEvent(NULL, FALSE, TRUE, L"dwm_event");
+		CK_TRACE_INFO("hzw:事件%x\n", hEvent);
+	}
+	else
+	{
+		//单线程跑 加不加都无所谓
+		//EnterSpinLock();
+		g_ImGuiDraw.ImGuiDx11Draw();
+		//LeaveSpinLock();
+	}
+
+}
+
+using PresentMPO_ = __int64( __fastcall* )( void*, IDXGISwapChain*, unsigned int, unsigned int, int, __int64, __int64, int );
+PresentMPO_ oPresentMPO = NULL;
+
+__int64 __fastcall hkPresentMPO( void* thisptr, IDXGISwapChain* pDxgiSwapChain, unsigned int a3, unsigned int a4, int a5, __int64 a6, __int64 a7, int a8 )
+{
+	DrawEverything(pDxgiSwapChain);
+	return oPresentMPO( thisptr, pDxgiSwapChain, a3, a4, a5, a6, a7, a8 );
+}
+
+using PresentDWM_ = __int64( __fastcall* )( void*, IDXGISwapChain*, unsigned int, unsigned int, const struct tagRECT*, unsigned int, const struct DXGI_SCROLL_RECT*, unsigned int, struct IDXGIResource*, unsigned int );
+PresentDWM_ oPresentDWM = NULL;
+
+__int64 __fastcall hkPresentDWM( void* thisptr, IDXGISwapChain* pDxgiSwapChain, unsigned int a3, unsigned int a4, const struct tagRECT* a5, unsigned int a6, const struct DXGI_SCROLL_RECT* a7, unsigned int a8, struct IDXGIResource* a9, unsigned int a10 )
+{
+	DrawEverything(pDxgiSwapChain);
+	return oPresentDWM( thisptr, pDxgiSwapChain, a3, a4, a5, a6, a7, a8, a9, a10 );
+}
+
+UINT WINAPI MainThread( PVOID )
+{
+	MH_Initialize();
+
+	while ( !GetModuleHandleA( "dwmcore.dll" ) )
+		Sleep( 150 );
+
+	//
+	// [ E8 ? ? ? ? ] the relative addr will be converted to absolute addr
+	auto ResolveCall = []( DWORD_PTR sig )
+	{
+		return sig = sig + *reinterpret_cast< PULONG >( sig + 1 ) + 5;
+	};
+
+	//
+	// [ 48 8D 05 ? ? ? ? ] the relative addr will be converted to absolute addr
+	auto ResolveRelative = []( DWORD_PTR sig )
+	{
+		return sig = sig + *reinterpret_cast< PULONG >( sig + 0x3 ) + 0x7;
+	};
+	
+	auto dwRender = FindPattern( "d2d1.dll", PBYTE( "\x48\x8D\x05\x00\x00\x00\x00\x33\xED\x48\x8D\x71\x08" ), "xxx????xxxxxx" );
+	if ( dwRender )
+	{
+		OutputDebugPrintf("hzw:d2d1.dll->dwRender:%llx\n", dwRender);
+		dwRender = ResolveRelative( dwRender );
+
+		PDWORD_PTR Vtbl = PDWORD_PTR( dwRender );
+		MH_CreateHook( PVOID( Vtbl[ 6 ] ), PVOID( &hkPresentDWM ), reinterpret_cast< PVOID* >( &oPresentDWM ) );
+		MH_CreateHook( PVOID( Vtbl[ 7 ] ), PVOID( &hkPresentMPO ), reinterpret_cast< PVOID* >( &oPresentMPO ) );
+		MH_EnableHook( MH_ALL_HOOKS );
+	}
+	return 0;
+}
+void DLLInitialization() {
+	_beginthreadex(nullptr, NULL, MainThread, nullptr, NULL, nullptr);
+}
+
+
+BOOL WINAPI DllMain( HMODULE hDll, DWORD dwReason, PVOID )
+{
+	if ( dwReason == DLL_PROCESS_ATTACH )
+	{
+			DLLInitialization();
+	}
+	return true;
+}
